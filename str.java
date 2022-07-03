041ebad57d30ffc6ab162d6b18c1c68f870603fa
MD SAMEER <sdukesameer@gmail.com>
2022-07-03 16:59:01 +0530

    str

--- str.java
+++ str.java
@@ -4,6 +4,6 @@ public class str {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-        
+        System.out.println(s);
     }
 }
